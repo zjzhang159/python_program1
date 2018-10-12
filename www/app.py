6511026ffbacc84f54624ea9f45e97bbb18f54be
zzj <1615138429@qq.com>
2018-10-12 11:08:37 +0800

    add content_type

--- www/app.py
+++ www/app.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
 @asyncio.coroutine
 def init(loop):
@@ -27,3 +27,4 @@ def init(loop):
 loop=asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
